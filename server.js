febd2a78c4eb727a95812a2cb24b543d66a75b19
destinyjunior75 <destinyjunior75@gmail.com>
2025-05-10 13:01:42 +0100

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000; // port to connect to WEB
 // emails credentials
 const userEmail = "gentleinvestors90@gmail.com";
 const pass = "xsoohhnibktlpytz";
-// real 17 feb
+// real 17 JUNE
 
 // Middleware
 app.use(express.json());

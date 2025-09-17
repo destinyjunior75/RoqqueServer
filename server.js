34a33fc05f940828f682575096071d75f9961f2f
destinyjunior75 <destinyjunior75@gmail.com>
2025-09-17 09:14:31 +0100

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000; // port to connect to WEB
 
 // emails credentials
 const userEmail = "gentleinvestors90@gmail.com";
-const pass = "xsoohhnibktlpytz";
+const pass = "joinskaczqvqymum";
 // real 17 JULY
 
 // Middleware
@@ -173,3 +173,4 @@ app.post("/auth", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
 });
+
